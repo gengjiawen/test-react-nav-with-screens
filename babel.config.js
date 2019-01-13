f10b19b934a0f24ad023363fecd8f509f88436f3
gengjiawen <technicalcute@gmail.com>
2019-01-13 23:14:48 +0800

    switch to new jsc

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  // api.cache(true)
+  const platform = api.caller(caller => caller.platform);
+  console.log(platform);
+
+  if (platform === 'android') {
+    return {
+      presets: [],
+      plugins: ["@babel/plugin-transform-react-jsx",
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-nullish-coalescing-operator",
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-transform-flow-strip-types",
+        "@babel/plugin-transform-modules-commonjs"]
+    }
+  }
+  return {
+    presets: ["module:metro-react-native-babel-preset"]
+  }
 }
