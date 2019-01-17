ab138c4c3d462f62fdbf1dba091f5080cc10c441
gengjiawen <technicalcute@gmail.com>
2019-01-17 22:20:52 +0800

    add more babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,17 @@
 module.exports = function (api) {
   // api.cache(true)
   const platform = api.caller(caller => caller.platform);
-  console.log(platform);
 
   if (platform === 'android') {
     return {
       presets: [],
       plugins: ["@babel/plugin-transform-react-jsx",
-        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-transform-flow-strip-types",
+        ["@babel/plugin-proposal-class-properties", {loose: true}],
+        "@babel/proposal-object-rest-spread",
+        "@babel/plugin-transform-destructuring",
         "@babel/plugin-proposal-nullish-coalescing-operator",
         "@babel/plugin-proposal-optional-chaining",
-        "@babel/plugin-transform-flow-strip-types",
         "@babel/plugin-transform-modules-commonjs"]
     }
   }
